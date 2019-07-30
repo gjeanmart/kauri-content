739522665a0fc6bd28e4d34a0cd6121b0d6533c3
gjeanmart <gregoire.jeanmart@gmail.com>
2019-07-30 17:14:43 +0100

    refactor config

--- spring-boot-simple/src/main/java/io/kauri/tutorial/springboot/simple/controllers/MessageController.java
+++ spring-boot-simple/src/main/java/io/kauri/tutorial/springboot/simple/controllers/MessageController.java
@@ -1,40 +1,38 @@
 package io.kauri.tutorial.springboot.simple.controllers;
 
-import java.util.Date;
 import java.util.List;
-
+import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import co.elastic.apm.api.CaptureTransaction;
 import io.kauri.tutorial.springboot.simple.model.Message;
-import io.kauri.tutorial.springboot.simple.repository.MessageRepository;
-import lombok.extern.slf4j.Slf4j;
+import io.kauri.tutorial.springboot.simple.services.MessageService;
 
 @RestController
-@Slf4j
 public class MessageController {
 
 	@Autowired
-	private MessageRepository repository;
+	private MessageService service;
 	
     @GetMapping("/message")
-    public List<Message> getMessages() {
-    	log.info("getting all messages");
-        return repository.findAll();
+    @CaptureTransaction(type = "request", value = "getMessages")
+    public List<Message> getMessages(
+    		@RequestParam(name = "error", defaultValue = "false") boolean error,
+    		@RequestParam(name = "sleep", required = false) Optional<Integer> sleep) {
+        return service.getMessages(error, sleep);
     }
     
     @PostMapping("/message")
-    public Message postMessage(@RequestBody String body) {
-    	Message message = new Message();
-    	message.setMessage(body);
-    	message.setPostedAt(new Date());
-    	
-    	log.info("posting {}", message);
-    	
-        return repository.save(message);
+    @CaptureTransaction(type = "request", value = "postMessage")
+    public Message postMessage(@RequestBody String body, 
+    		@RequestParam(name = "error", defaultValue = "false") boolean error,
+    		@RequestParam(name = "sleep", required = false) Optional<Integer> sleep) {
+        return service.postMessage(body, error, sleep);
     }
     
 }
---
+++ spring-boot-simple/src/main/java/io/kauri/tutorial/springboot/simple/services/MessageService.java
@@ -0,0 +1,81 @@
+package io.kauri.tutorial.springboot.simple.services;
+
+import java.util.Date;
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Service;
+
+import co.elastic.apm.api.CaptureSpan;
+import co.elastic.apm.api.CaptureTransaction;
+import io.kauri.tutorial.springboot.simple.model.Message;
+import io.kauri.tutorial.springboot.simple.repository.MessageRepository;
+import lombok.extern.slf4j.Slf4j;
+
+@Service
+@Slf4j
+public class MessageService {
+
+	@Autowired
+	private MessageRepository repository;
+	
+    @Scheduled(fixedDelayString = "60000")
+    public void execute() {
+        log.info("run scheduled test");
+        doExecute();
+    }
+    
+    @CaptureTransaction(type = "task", value = "ProcessNewUsers")
+    private void doExecute() {
+    	repository.findAll().forEach(msg-> log.debug(msg.getMessage()));
+    }
+	
+    @CaptureSpan(type = "app", value = "getMessages")
+    public List<Message> getMessages(boolean error, Optional<Integer> sleep) {
+    	try {
+        	log.info("getting all messages...");
+        	
+        	if(error)
+        		throw new Exception("Random error");
+        	
+        	if(sleep.isPresent())
+        		Thread.sleep(sleep.get());
+        	
+            return repository.findAll();
+    		
+    	} catch(Exception ex) {
+    		log.error("Error {}", ex.getMessage(), ex);
+    		throw new RuntimeException(ex);
+    	}
+    }
+
+    @CaptureSpan(type = "app", value = "postMessage")
+    public Message postMessage(String body, boolean error, Optional<Integer> sleep) {
+    	
+    	try {
+        	Message message = new Message();
+        	message.setMessage(body);
+        	message.setPostedAt(new Date());
+        	
+        	log.info("posting {} ...", message);
+
+        	if(error)
+        		throw new Exception("Random error");
+        	
+        	if(sleep.isPresent())
+        		Thread.sleep(sleep.get());
+        	
+        	message = repository.save(message);
+        	
+        	log.info("posted {} !", message);
+        	
+        	return message;
+    		
+    	} catch(Exception ex) {
+    		log.error("Error {}", ex.getMessage(), ex);
+    		throw new RuntimeException(ex);
+    	}
+    }
+}
